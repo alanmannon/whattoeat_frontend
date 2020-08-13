e0a0f642b1b49a9ba96f8603e5df3df1f66e9517
Alan <alanmannon@gmail.com>
2020-08-12 21:24:53 -0500

    created restaurantindex page

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,11 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from '../views/Home.vue';
+import RestaurantIndex from '../views/RestaurantIndex.vue';
 
 Vue.use(VueRouter)
 
-  const routes = [
+const routes = [
   {
     path: '/',
     name: 'Home',
@@ -17,13 +18,18 @@ Vue.use(VueRouter)
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+  },
+  {
+    path: '/index',
+    name: 'Restaurant Index',
+    component: RestaurantIndex
+  },
+];
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
-})
+});
 
-export default router
+export default router;
---
+++ src/views/RestaurantIndex.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+    <h3>{{ index }}</h3>
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      message: "Restaurants Near You",
+      index: [],
+    };
+  },
+  created: function () {
+    this.zomatoIndex();
+  },
+  methods: {
+    zomatoIndex: function () {
+      axios
+        .get(
+          "https://developers.zomato.com/api/v2.1/location_details?entity_id=5200&entity_type=city",
+          { headers: { "user-key": "API KEY API KEY API KEY" } }
+        )
+        .then((response) => {
+          this.index = response.data;
+        });
+    },
+  },
+};
+</script>
