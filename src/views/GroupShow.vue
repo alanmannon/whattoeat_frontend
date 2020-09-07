2abde621e4caca2f3c6a2c3ff36ac9c8bf50f42c
Alan <alanmannon@gmail.com>
2020-09-06 21:59:30 -0500

    started Groupshow page

--- src/views/GroupShow.vue
+++ src/views/GroupShow.vue
@@ -1,6 +1,24 @@
 <template>
   <div class="home">
-    <h1>{{ message }}</h1>
+    <div class="container" align="center">
+      <h1> {{ message }} </h1>
+      <div class="card-group" align="center">
+        <div v-for='group in groups["best_rated_restaurant"]'>
+          <div class="card h-100" style="width: 20rem;">
+            <img class="card-img-top" src="">
+            <div class="card-body">
+              <h5 class="card-title">{{group}}</h5>
+              <p class="card-text">{{ restaurant["restaurant"]["cuisines"] }}</p>
+              <a v-bind:href='`/show?restid=${restaurant["restaurant"]["id"]}`' class="btn btn-primary">More Info</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    
+  
+
+    
+    </div>
   </div>
 </template>
 
@@ -8,13 +26,26 @@
 </style>
 
 <script>
+import axios from "axios";
 export default {
   data: function () {
     return {
       message: "Welcome to Vue.js!",
+      groups: [],
     };
   },
-  created: function () {},
-  methods: {},
+  created: function () {
+    this.getGroup();
+  },
+  methods: {
+    getGroup: function () {
+      axios
+        .get(`/api/usergroup?group_id=${this.$route.query.group_id}`)
+        .then((response) => {
+          this.group = response.data;
+          console.log(response.data);
+        });
+    },
+  },
 };
-</script>
+</script>``
